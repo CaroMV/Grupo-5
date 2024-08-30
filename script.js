0ed3d74648701856a95ad0254864549503f40f71
unknown <cmoreno@codingdojo.la>
2024-08-30 01:03:42 -0400

    archivos sin corrección

---
+++ script.js
@@ -0,0 +1,11 @@
+function usua(){
+    window.location.href= "index2.html"
+}
+
+function crear(){
+    window.location.href= "index3.html"
+}
+
+function iniciar(){
+    window.location.href= "index2.html"
+}
